3194ea0c77619d606019a45394bbb38055f5e4ec
azhariamir01 <53530910+azhariamir01@users.noreply.github.com>
2021-05-12 20:51:37 +0300

    Add files via upload

---
+++ companyActions/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from authentication import views as aut
+
+urlpatterns = [
+    path('homecompany/', views.homecompany, name='homecompany'),
+    path('addshares/', views.addshares, name='addshares'),
+    path('removeshares/', views.removeshares, name='removeshares'),
+]
