508eee6b32223289baa654e38c85a80824cb55d0
azhariamir01 <53530910+azhariamir01@users.noreply.github.com>
2021-05-16 13:18:53 +0300

    Add files via upload

--- companyActions/urls.py
+++ companyActions/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('homecompany/', views.homecompany, name='homecompany'),
     path('addshares/', views.addshares, name='addshares'),
     path('removeshares/', views.removeshares, name='removeshares'),
+    path('info/', views.info, name='info'),
 ]
